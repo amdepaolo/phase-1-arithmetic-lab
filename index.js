09b5615734b6df1c5f6c74a4ed80206c364ff4bf
Adam DePaolo <amdepaolo@gmail.com>
2022-03-16 14:34:50 -0400

    commit tree 5ea24c72d8b129043548edfc0afe380cf929dd22

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2
 
-// Write your code here
+let random = Math.ceil(Math.random() * 10);
+
+let num3 = 104;
+let num4 = 10;
+
+let mod = num3 % num4;
+
+let max = Math.max(1,2,3,4,20);
